40cf76d923d7497055169c411864954d96d76edb
mohdfahoum <mohd.fahoum@gmail.com>
2020-07-11 11:28:56 +0300

    firstQuestion

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -8,8 +8,13 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+var a = 4;
+var b = 7;
 
+function sum(a, b) { //eslint-disable-line
+    var sum = a+b ;
+    var returnArray = [sum , 'the sum of '+a+ 'and '+b+ 'is '+sum]
+return returnArray;
 }
 
 // Here is the test for sum(); uncomment it to run it
